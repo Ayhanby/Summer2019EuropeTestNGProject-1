2e426698c9b2a6db40c5cbe49b660827560da0ff
Harun <hekinci11@gmail.com>
2020-01-10 00:41:40 +0300

    DDF

---
+++ src/test/java/com/cybertek/tests/day17_ddf/ExcelUtilsDemo.java
@@ -0,0 +1,48 @@
+package com.cybertek.tests.day17_ddf;
+
+import com.cybertek.utilities.ExcelUtil;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class ExcelUtilsDemo {
+
+    @Test
+    public void readExcelFile(){
+
+        // create an object from ExcelUtil
+        //it accepts two argument
+        //Argument 1: location of the file(path)
+        // Argument 2: sheet that we want to open(sheetName)
+
+        ExcelUtil qa3short = new ExcelUtil("src/test/resources/Vytracktestdata.xlsx","QA3-short");
+
+        // how many rows in the sheet
+        System.out.println("Row count: "+ qa3short.rowCount());
+
+        // how many column is in the sheet
+        System.out.println("Columns count: "+ qa3short.columnCount());
+        // get all column names
+        System.out.println("Column names"+ qa3short.getColumnsNames());
+        System.out.println( "Column names: "+ qa3short.getColumnsNames().get(0));
+
+        // get all dataline list of maps
+        List<Map<String, String>> dataList = qa3short.getDataList();
+        // print them one by one for each row
+        for (Map<String,String>onerow:dataList){
+            System.out.println(onerow);
+        }
+       // System.out.println(dataList.get(0).get("firstname"));
+
+        // get all data in 2d array
+        String[][] dataArray=qa3short.getDataArray();
+        // print the array
+        System.out.println(Arrays.deepToString(dataArray));
+
+
+    }
+
+
+}
